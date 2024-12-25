983f7bcd7d50d83f7f2a4ad029f703e1ec92c83a
Prince <princemehta76@gmail.com>
2024-12-25 21:47:56 +0530

    added fibonacci for nth number

---
+++ Recursion/NFibonacci.java
@@ -0,0 +1,15 @@
+package Recursion;
+//Print Nth Fibonacci number
+public class NFibonacci {
+    public static int fibonacci(int n){
+        if(n == 0 || n == 1){//Base Case
+            return n;
+        }
+        int fib = fibonacci(n-1) + fibonacci(n-2);
+        return fib;
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(fibonacci(n));
+    }
+}
